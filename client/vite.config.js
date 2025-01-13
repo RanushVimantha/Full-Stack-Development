ac73e88a3e2ecce55f01b13a1422060c6bba193e
Ranush Vimantha <76447833+RanushVimantha@users.noreply.github.com>
2025-01-13 20:37:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'esnext', // Ensure modern JavaScript compatibility
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Automatically resolve these extensions
-  },
+	plugins: [react()],
+	build: {
+		target: 'esnext',
+		outDir: 'dist'
+	},
 });
